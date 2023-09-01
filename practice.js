3d802fe7722d8e37ea68fd9b12ef522db029f533
Naeem Hossen <132334814+NHossen@users.noreply.github.com>
2023-09-01 20:34:24 +0400

    Blog page upgraded

--- practice.js
+++ practice.js
@@ -1,53 +1,113 @@
-// Define a global variable to store the data.
-let currentData = [];
 
-// Function to fetch and render tab categories.
+let currentData = [];//Delare variable to keap value of function
+
+
 const handelTabContainer = async () => {
-  // Fetch categories data.
   const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
   const data = await res.json();
-
-  // Get the category data.
+ //console.log(data);
   const getData = data.data;
   const tabContainer = document.getElementById('tab-container');
+  tabContainer.innerHTML = "";// Clear previous data.
 
-  // Clear previous data.
-  tabContainer.innerHTML = "";
-
-  // Iterate through categories and create tab links.
-  getData.forEach((dataId) => {
+  
+  getData.forEach((dataId) => {// create tab links.
     const newDiv = document.createElement('div');
     newDiv.innerHTML = `
       <a onclick="handelPostContainer('${dataId.category_id}')" class="tab tab-active bg-[#d4d4d4] rounded-lg">${dataId.category}</a>
     `;
     tabContainer.appendChild(newDiv);
   });
-
-  // Store the fetched data in the global variable currentData.
-  currentData = getData;
+  currentData = getData;//Stor data in let variable
 }
 
-// Function to fetch and render posts based on category.
-const handelPostContainer = async (getDataId) => {
-  // Fetch posts data for a specific category.
+
+const handelPostContainer = async (getDataId) => {//get data by Id
   const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${getDataId}`);
   const data = await res.json();
+  const newData = data.data;  // Get the post data.
+  //console.log(newData);
 
-  // Get the post data.
-  const newData = data.data;
   const cardContainer = document.getElementById('card-container');
   cardContainer.innerHTML = "";
 
-  // Check if there is no data.
-  if (newData.length === 0) {
+ 
+  if (newData.length === 0) {//for emty data container
     cardContainer.innerHTML = `
-      <p>No data found</p> <img src="icon.png"/>
+    <div class="absolute">
+      <img class=" lg:ml-[500px] ml-[100px] lg:w-80 " src="icon.png"/>
+       <p class="lg:ml-[500px] ml-[25px] lg:mt-10 text-2xl text-center font-bold">Oops!! Sorry, There is no <br> content here</p>
+    </div>
+     
     `;
   } else {
+    
+    newData.forEach((dataId) => {// data for each card
+      const postedDateInSeconds = dataId?.others.posted_date;
+      const hours = Math.floor(postedDateInSeconds / 3600);
+      const minutes = Math.floor((postedDateInSeconds % 3600) / 60);
+
+      let formattedPostedDate = "";//data posted time 
+      if (hours > 0) {
+        formattedPostedDate = `${hours} hr${hours > 1 ? "s" : ""}`;
+        if (minutes > 0) {
+          formattedPostedDate += ` ${minutes} min ago${minutes > 1 ? "" : ""}`;
+        }
+      } else {
+        formattedPostedDate = "";
+      }
+
+      const newDiv = document.createElement("div");//Create new element to keep card
+      newDiv.innerHTML = `
+        <div class="card card-compact">
+          <div class=" ">
+            <figure><img class="w-full h-[180px] mx-auto rounded-lg " src="${dataId.thumbnail}" alt="Shoes" /></figure>
+            <div class="flex justify-end mr-2">
+              <p class="-mt-8 px-2  absolute bg-[#0c0c0c] rounded-lg text-[white]">${formattedPostedDate} </p>
+            </div>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="flex gap-2 items-center">
+            <img class="w-10 h-10 rounded-full " src="${dataId.authors[0].profile_picture}" alt="">
+            <h2 class="card-title text-xl font-bold">${dataId.title}</h2>
+          </div>
+          <div>
+            <p class="flex items-center text-lg gap-4">${dataId.authors[0].profile_name} <img class="w-[20px" src="${dataId?.authors[0]?.verified ? dataId.authors[0].verified : 'seal.svg'}" alt="">
+            ${dataId?.authors[0]?.isVerified ? '' : ''}
+            </p>
+          </div>
+          <p>${dataId?.others.views}</p>
+        </div>
+      `;
+
+      cardContainer.appendChild(newDiv);
+    });
+  }
 
 
-    // Render the sorted data.
-    newData.forEach((dataId) => {
+  currentData = newData;//
+}
+
+
+const sortDataByViews = () => { //Sort data by views 
+  if (currentData.length > 0) {
+    currentData.sort((a, b) => parseInt(b.others.views, 10) - parseInt(a.others.views, 10));
+    renderData(currentData);//get data from let varibale
+  }
+}
+
+const renderData = (data) => {//Sorting for each item 
+  const cardContainer = document.getElementById('card-container');
+  cardContainer.innerHTML = "";
+
+  if (data.length === 0) {
+    cardContainer.innerHTML = `
+      <p class="text-right">No data found</p>
+       <img src="icon.png"/>
+    `;
+  } else {
+    data.forEach((dataId) => {
       const postedDateInSeconds = dataId?.others.posted_date;
       const hours = Math.floor(postedDateInSeconds / 3600);
       const minutes = Math.floor((postedDateInSeconds % 3600) / 60);
@@ -90,46 +150,12 @@ const handelPostContainer = async (getDataId) => {
       cardContainer.appendChild(newDiv);
     });
   }
-
-  // Store the fetched data in the global variable currentData.
-  currentData = newData;
 }
 
-// Function to sort data by views and re-render.
-const sortDataByViews = () => {
-
-    console("cliked")
-  // Check if there is data to sort.
-  if (currentData.length > 0) {
-    // Sort currentData by views in descending order (highest views first).
-    currentData.sort((a, b) => parseInt(b.others.views, 10) - parseInt(a.others.views, 10));
-
-    // Re-render the sorted data.
-    renderData(currentData);
-  }
-}
-
-// Function to render data.
-const renderData = (data) => {
-  const cardContainer = document.getElementById('card-container');
-  cardContainer.innerHTML = "";
-
-  if (data.length === 0) {
-    cardContainer.innerHTML = `
-      <p>No data found</p> <img src="icon.png"/>
-    `;
-  } else {
-    data.forEach((dataId) => {
-      // ... Your existing code for rendering each post ...
-    });
-  }
-}
-
-// Add an event listener to the "Sort by View" button.
 const sortButton = document.getElementById('sort-btn');
 const sortBtn = sortButton.querySelector('button');
-sortBtn.addEventListener('click', sortDataByViews);
+sortBtn.addEventListener('click', sortDataByViews);//add button clicke to sort data
 
-// Call the initial functions to fetch and render data.
+
 handelTabContainer();
 handelPostContainer('1000');
--- script.js
+++ script.js
@@ -1,82 +1,77 @@
 
-const handelTabContainer=async()=>{
-    const res= await  fetch(" https://openapi.programming-hero.com/api/videos/categories");
-    const data=await res.json();
-   //console.log(data.data[1].category);
-    const getData=data.data;
-    //console.log(getData);
-   const tabContainer=document.getElementById('tab-container')
-    getData.forEach((dataId)=>{//get data id by forEach loop
-        //console.log(dataId);
-        const newDiv=document.createElement('div');
+let currentData = [];//Delare variable to keap value of function
 
-        newDiv.innerHTML=`
-        <a onclick="handelPostContainer('${dataId.category_id}')" class="tab tab-active bg-[#d4d4d4] rounded-lg">${dataId.category}</a>
-        `;
 
-        tabContainer.appendChild(newDiv);
-        console.log("news data",dataId.category_id)
-    })
+const handelTabContainer = async () => {
+  const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
+  const data = await res.json();
+ //console.log(data);
+  const getData = data.data;
+  const tabContainer = document.getElementById('tab-container');
+  tabContainer.innerHTML = "";// Clear previous data.
+
+  
+  getData.forEach((dataId) => {// create tab links.
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+      <a onclick="handelPostContainer('${dataId.category_id}')" class="tab tab-active bg-[#d4d4d4] rounded-lg">${dataId.category}</a>
+    `;
+    tabContainer.appendChild(newDiv);
+  });
+  currentData = getData;//Stor data in let variable
 }
 
-const handelPostContainer=async (getDataId)=>{
-    //console.log(getDataId);
-    const res=await fetch(`https://openapi.programming-hero.com/api/videos/category/${getDataId}`);
-    const data=await res.json();
-    const newData=data.data;
-    
-    const cardContainer=document.getElementById('card-container');
-    cardContainer.innerHTML="";
-    
-    //console.log("new data",newData);//important
- if(newData.length === 0 ){
-    cardContainer.innerHTML = `
-    <div class="border-2">
-     <img class="w-[800px] mx-auto" src="icon.png"/>
-     <p class="w-[400px]">No data found</p>
-    </div>
-   
-    
-    `;
- }else{
-newData.forEach((dataId)=>{
-        //console.log(dataId.thumbnail);
-        //  Posted date
-         // Convert seconds to hours and minutes
-         const postedDateInSeconds = dataId?.others.posted_date;
-         const hours = Math.floor(postedDateInSeconds / 3600);
-         const minutes = Math.floor((postedDateInSeconds % 3600) / 60);
-         // Format the posted date
-         let formattedPostedDate = "";
+
+const handelPostContainer = async (getDataId) => {//get data by Id
+  const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${getDataId}`);
+  const data = await res.json();
+  const newData = data.data;  // Get the post data.
+  //console.log(newData);
+
+  const cardContainer = document.getElementById('card-container');
+  cardContainer.innerHTML = "";
+
  
-         if (hours > 0) {
-             formattedPostedDate = `${hours} hr${hours > 1 ? "s" : ""}`;
-             if (minutes > 0) {
-                 formattedPostedDate += ` ${minutes} min ago${minutes > 1 ? "" : ""}`;
-             }
-         }
-          else {
-             formattedPostedDate = "";
-         }
+  if (newData.length === 0) {//for emty data container
+    cardContainer.innerHTML = `
+    <div class="absolute">
+      <img class=" lg:ml-[500px] ml-[100px] lg:w-80 " src="icon.png"/>
+       <p class="lg:ml-[500px] ml-[25px] lg:mt-10 text-2xl text-center font-bold">Oops!! Sorry, There is no <br> content here</p>
+    </div>
+     
+    `;
+  } else {
+    
+    newData.forEach((dataId) => {// data for each card
+      const postedDateInSeconds = dataId?.others.posted_date;
+      const hours = Math.floor(postedDateInSeconds / 3600);
+      const minutes = Math.floor((postedDateInSeconds % 3600) / 60);
 
- const newDiv=document.createElement("div");
+      let formattedPostedDate = "";//data posted time 
+      if (hours > 0) {
+        formattedPostedDate = `${hours} hr${hours > 1 ? "s" : ""}`;
+        if (minutes > 0) {
+          formattedPostedDate += ` ${minutes} min ago${minutes > 1 ? "" : ""}`;
+        }
+      } else {
+        formattedPostedDate = "";
+      }
 
-        newDiv.innerHTML= `
+      const newDiv = document.createElement("div");//Create new element to keep card
+      newDiv.innerHTML = `
         <div class="card card-compact">
-        <div class=" ">
-        <figure><img class="w-full h-[180px] mx-auto rounded-lg " src="${dataId.thumbnail}" alt="Shoes" /></figure>
-        <div class="flex justify-end mr-2">
-         <p class="-mt-8 px-2  absolute bg-[#0c0c0c] rounded-lg text-[white]">${formattedPostedDate} </p>
-        </div>
-        </div>
+          <div class=" ">
+            <figure><img class="w-full h-[180px] mx-auto rounded-lg " src="${dataId.thumbnail}" alt="Shoes" /></figure>
+            <div class="flex justify-end mr-2">
+              <p class="-mt-8 px-2  absolute bg-[#0c0c0c] rounded-lg text-[white]">${formattedPostedDate} </p>
+            </div>
+          </div>
         </div>
         <div class="card-body">
-            <!-- Post title -->
-            <div class="flex gap-2 items-center">
-                <img class="w-10 h-10 rounded-full " src="${dataId.authors[0].profile_picture}" alt="">
-                <h2 class="card-title text-xl font-bold">${dataId.title}</h2>
-            </div>
-           <!-- Author Name  -->
+          <div class="flex gap-2 items-center">
+            <img class="w-10 h-10 rounded-full " src="${dataId.authors[0].profile_picture}" alt="">
+            <h2 class="card-title text-xl font-bold">${dataId.title}</h2>
+          </div>
           <div>
             <p class="flex items-center text-lg gap-4">${dataId.authors[0].profile_name} <img class="w-[20px" src="${dataId?.authors[0]?.verified ? dataId.authors[0].verified : 'seal.svg'}" alt="">
             ${dataId?.authors[0]?.isVerified ? '' : ''}
@@ -84,28 +79,83 @@ newData.forEach((dataId)=>{
           </div>
           <p>${dataId?.others.views}</p>
         </div>
-    </div>
-        
-        `;
-    cardContainer.appendChild(newDiv);
-    console.log("this is data",dataId); 
+      `;
 
-    
+      cardContainer.appendChild(newDiv);
     });
+  }
 
- }
 
- 
-     
+  currentData = newData;//
 }
 
 
-
-// handelSort//  newData.sort((a, b) => parseInt(b.others.views, 10) - parseInt(a.others.views, 10));
-const sortDataByViews=(newData)=>{
-    console.log("this is new data section",newData)
-    //newData.sort((a, b) => parseInt(b.others.views, 10) - parseInt(a.others.views, 10));
+const sortDataByViews = () => { //Sort data by views 
+  if (currentData.length > 0) {
+    currentData.sort((a, b) => parseInt(b.others.views, 10) - parseInt(a.others.views, 10));
+    renderData(currentData);//get data from let varibale
+  }
 }
 
-handelPostContainer('1000')
-handelTabContainer()
+const renderData = (data) => {//Sorting for each item 
+  const cardContainer = document.getElementById('card-container');
+  cardContainer.innerHTML = "";
+
+  if (data.length === 0) {
+    cardContainer.innerHTML = `
+      <p class="text-right">No data found</p>
+       <img src="icon.png"/>
+    `;
+  } else {
+    data.forEach((dataId) => {
+      const postedDateInSeconds = dataId?.others.posted_date;
+      const hours = Math.floor(postedDateInSeconds / 3600);
+      const minutes = Math.floor((postedDateInSeconds % 3600) / 60);
+
+      let formattedPostedDate = "";
+
+      if (hours > 0) {
+        formattedPostedDate = `${hours} hr${hours > 1 ? "s" : ""}`;
+        if (minutes > 0) {
+          formattedPostedDate += ` ${minutes} min ago${minutes > 1 ? "" : ""}`;
+        }
+      } else {
+        formattedPostedDate = "";
+      }
+
+      const newDiv = document.createElement("div");
+      newDiv.innerHTML = `
+        <div class="card card-compact">
+          <div class=" ">
+            <figure><img class="w-full h-[180px] mx-auto rounded-lg " src="${dataId.thumbnail}" alt="Shoes" /></figure>
+            <div class="flex justify-end mr-2">
+              <p class="-mt-8 px-2  absolute bg-[#0c0c0c] rounded-lg text-[white]">${formattedPostedDate} </p>
+            </div>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="flex gap-2 items-center">
+            <img class="w-10 h-10 rounded-full " src="${dataId.authors[0].profile_picture}" alt="">
+            <h2 class="card-title text-xl font-bold">${dataId.title}</h2>
+          </div>
+          <div>
+            <p class="flex items-center text-lg gap-4">${dataId.authors[0].profile_name} <img class="w-[20px" src="${dataId?.authors[0]?.verified ? dataId.authors[0].verified : 'seal.svg'}" alt="">
+            ${dataId?.authors[0]?.isVerified ? '' : ''}
+            </p>
+          </div>
+          <p>${dataId?.others.views}</p>
+        </div>
+      `;
+
+      cardContainer.appendChild(newDiv);
+    });
+  }
+}
+
+const sortButton = document.getElementById('sort-btn');
+const sortBtn = sortButton.querySelector('button');
+sortBtn.addEventListener('click', sortDataByViews);//add button clicke to sort data
+
+
+handelTabContainer();
+handelPostContainer('1000');
